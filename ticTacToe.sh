81b1ac5070c6ea110f1b37a9a317b838d87c90f6
seema1611 <seemarajpure16@gmail.com>
2020-04-13 20:04:33 +0530

    ADDED: Function to assign symbol

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -13,4 +13,20 @@ function reset(){
       gameBoard[i]=" "
    done
 }
+
+#Function to assign symbol for player and computer
+function assignSymbol() {
+   if [ $((RANDOM%2)) -eq 0 ]
+   then
+      playerMark=X
+      computerMark=0
+   else
+      playerMark=0
+      computerMark=X
+   fi
+
+   echo "Player Mark:"  $playerMark
+   echo "Computer Mark:"  $computerMark
+}
 reset
+assignSymbol
